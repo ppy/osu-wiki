54ee157b0e37f335c7cbe13d1941601673bf55f2
clayton <me@clayton.cc>
2019-05-27 19:58:51 -0700

    Add summary of changes for each rule

--- bin/format.js
+++ bin/format.js
@@ -16,10 +16,26 @@ const rulesToCheck = ruleArgIndex === 0 ? null : process.argv[ruleArgIndex].spli
 
 articles.forEach(article => format(article));
 
-const changeCount = Object.values(changes).reduce((sum, c) => sum + c, 0);
-const changeFileCount = Object.values(changes).filter(c => c > 0).length;
+let changeCountTotal = 0;
+let changedFilesTotal = [];
 
-console.log(`\x1b[30m\x1b[42mDONE!\x1b[0m Made \x1b[32m${changeCount}\x1b[0m changes to \x1b[32m${changeFileCount} / ${articles.length}\x1b[0m articles`);
+for (let i = 0; i < rules.length; i++) {
+    if (changes[i] === undefined)
+        continue;
+
+    const changeCount = Object.values(changes[i]).reduce((sum, c) => sum + c, 0);
+    const changedFiles = Object.entries(changes[i]).filter(c => c[1] > 0).map(c => c[0]);
+    const indent = ' '.repeat(4 - i.toString().length);
+
+    changeCountTotal += changeCount;
+    changedFilesTotal = changedFilesTotal.concat(changedFiles);
+
+    console.log(`\x1b[30m\x1b[43mRULE ${i}\x1b[0m${indent}Made \x1b[33m${changeCount.toString().padStart(3)}\x1b[0m changes to \x1b[33m${changedFiles.length.toString().padStart(3)}\x1b[0m articles`);
+};
+
+const changedFilesCount = changedFilesTotal.filter((f, i, files) => files.indexOf(f) === i).length;
+
+console.log(`\x1b[30m\x1b[42mDONE!\x1b[0m    Made \x1b[32m${changeCountTotal.toString().padStart(3)}\x1b[0m changes to \x1b[32m${changedFilesCount.toString().padStart(3)} / ${articles.length}\x1b[0m articles`);
 
 function rule_1_boundaryWhitesapce(content, meta, log) {
     const original = '' + content;
@@ -88,7 +104,6 @@ function findArticles(dir) {
 }
 
 function format(file) {
-    const log = logFactory(file);
     let content = fs.readFileSync(file, 'utf8');
     let meta;
 
@@ -105,9 +120,15 @@ function format(file) {
 
     [content, meta] = detachMeta(content);
 
-    rules
-        .filter((rule, index) => rule !== null && (rulesToCheck === null || rulesToCheck.includes(index)))
-        .forEach(rule => [content, meta] = rule(content, meta, log));
+    rules.forEach((rule, index) => {
+        if (rule === null || (rulesToCheck !== null && !rulesToCheck.includes(index)))
+            return;
+
+        if (changes[index] === undefined)
+            changes[index] = {};
+
+        [content, meta] = rule(content, meta, logFactory(file, index));
+    });
 
     content = attachMeta(content, meta);
 
@@ -132,14 +153,14 @@ function lineAtIndex(string, index) {
     return lineNumber;
 }
 
-function logFactory(file) {
+function logFactory(file, ruleNumber) {
     file = file.substring(path.join(__dirname, '..').length);
 
-    if (changes[file] === undefined)
-        changes[file] = 0;
+    if (changes[ruleNumber][file] === undefined)
+        changes[ruleNumber][file] = 0;
 
     return (line, message) => {
         console.log(`${file} (line ${line}): \x1b[31m${message}\x1b[0m`);
-        ++changes[file];
+        ++changes[ruleNumber][file];
     }
 }
