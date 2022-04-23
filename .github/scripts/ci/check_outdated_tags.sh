d3fdb2a0a4aa65df1fd394d3030d37b4a1131c47
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-04-23 09:14:00 +0200

    adjust scripts to make them work

--- .github/scripts/ci/check_outdated_tags.sh
+++ .github/scripts/ci/check_outdated_tags.sh
@@ -1,8 +1,17 @@
+MASK='---\n(.*\n)*?((outdated: |outdated_since: ).+\n)(.*\n)*?---'
+PULL_REQUEST_TAG='SKIP_OUTDATED_CHECK'
+
+FIRST_COMMIT_HASH=$1
+LAST_COMMIT_HASH=$2
+FIRST_PR_COMMIT_HASH=$3
+
 # https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
 function echo_red () { echo -e "\e[0;31m$1\e[m"; }
 function echo_green () { echo -e "\e[0;32m$1\e[m"; }
+function echo_grey () { echo -e "\e[0;90m$1\e[m"; }
+
 function print_error () {
-  echo "::error::You have edited some original articles (en.md), but did not outdate their translations:"
+  echo "$( echo_red 'Error:' ) You have edited some original articles (en.md), but did not outdate their translations:"
   while read FILENAME; do
     echo_red "* $FILENAME"
   done <<< "$1"
@@ -15,17 +24,16 @@ function print_error () {
   fi
   echo_green "---"
 }
-function diff_files () { git diff --diff-filter=d --name-only ${{ github.sha }}^ ${{ github.sha }} "$@"; }
+
+function diff_files () { git diff --diff-filter=d --name-only ${FIRST_COMMIT_HASH}^ ${LAST_COMMIT_HASH} "$@"; }
+
 TRANSLATIONS=$( diff_files 'wiki/**/*.md' ':(exclude)*/en.md' )
 ORIGINAL_ARTICLES=$( diff_files 'wiki/**/en.md' )
 if [[ -z "$ORIGINAL_ARTICLES" ]]; then
-  echo "::notice::No original articles are edited, exiting"
+  echo "$( echo_grey 'Notice:' ) No original articles are edited, exiting"
   exit 0
 fi
-if ${{ contains(github.event.pull_request.body, env.PULL_REQUEST_TAG) }}; then
-  echo "::notice::Outdated articles check suppressed ($PULL_REQUEST_TAG tag found in the pull request)"
-  exit 0
-fi
+
 # obtain directories of the modified en.md files, then list translations in them which:
 # - are not modified in the PR (assuming that the author took care of them)
 # - don't have the outdated/outdated_since markers
@@ -41,11 +49,16 @@ MISSED_TRANSLATIONS=$( echo "$ORIGINAL_ARTICLES" | tr \\n \\0 | xargs -0 dirname
     done
   done
 })
+
 if [[ -n "$MISSED_TRANSLATIONS" ]]; then
-  # get the first commit of the branch associated with the PR; GitHub's ubuntu-latest has curl/jq: https://github.com/actions/virtual-environments
-  FIRST_COMMIT_HASH=$( curl -sS ${{ github.event.pull_request.commits_url }}?per_page=1 | jq -r '.[0].sha' || true )
-  print_error "$MISSED_TRANSLATIONS" "$FIRST_COMMIT_HASH"
+  if [[ $FIRST_PR_COMMIT_HASH ]]; then
+    # GitHub Action host
+    print_error "$MISSED_TRANSLATIONS" "$FIRST_PR_COMMIT_HASH"
+  else
+    # Local run via run-ci.sh
+    print_error "$MISSED_TRANSLATIONS" "$FIRST_COMMIT_HASH"
+  fi
   exit 1
 else
-  echo -e "::notice::Either you have edited no original articles, or all translations are properly outdated"
+  echo -e "$( echo_grey 'Notice:' ) Either you have edited no original articles, or all translations are properly outdated"
 fi
