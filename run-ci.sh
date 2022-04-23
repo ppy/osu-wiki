0eb9ab27ada35212a3c53e317a0d962bf3f27708
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-04-23 09:09:10 +0200

    add script for running ci locally

---
+++ run-ci.sh
@@ -0,0 +1,20 @@
+FIRST_COMMIT_HASH=$( git log master..$( git branch --show-current ) --pretty=oneline | tail -1 | awk '{ print $1 }' )
+LAST_COMMIT_HASH=$( git rev-parse HEAD )
+ARTICLES=$( git diff --diff-filter=d --name-only ${FIRST_COMMIT_HASH}^ ${LAST_COMMIT_HASH} "wiki/**/*.md" "news/*.md" )
+
+echo -e "\n--- Binary file size check ---\n"
+bash .github/scripts/ci/inspect_binary_file_sizes.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+
+echo -e "\n--- Run remark ---\n"
+bash .github/scripts/ci/run_remark.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+
+echo -e "\n--- Run yamllint ---\n"
+python .github/scripts/ci/run_yamllint.py --config .yamllint.yaml
+
+echo -e "\n--- Broken wikilink check ---\n"
+python .github/scripts/ci/find_broken_wikilinks.py --target ${ARTICLES}
+
+echo -e "\n--- Outdated tag check ---\n"
+bash .github/scripts/ci/check_outdated_tags.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+
+echo
