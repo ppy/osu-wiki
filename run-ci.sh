798a37b0dde93274c9e0b4fc22768200a76120f7
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-04-25 20:59:34 +0200

    move scripts to top-level folder

--- run-ci.sh
+++ run-ci.sh
@@ -5,16 +5,16 @@ LAST_COMMIT_HASH=$( git rev-parse HEAD )
 ARTICLES=$( git diff --diff-filter=d --name-only ${FIRST_COMMIT_HASH}^ ${LAST_COMMIT_HASH} "wiki/**/*.md" "news/*.md" )
 
 printf -- "--- Binary file size check ---\n\n"
-bash .github/scripts/ci/inspect_binary_file_sizes.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+bash scripts/ci/inspect_binary_file_sizes.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
 
 printf -- "\n--- Run remark ---\n\n"
-bash .github/scripts/ci/run_remark.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+bash scripts/ci/run_remark.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
 
 printf -- "\n--- Run yamllint ---\n\n"
-python3 .github/scripts/ci/run_yamllint.py --config .yamllint.yaml
+python3 scripts/ci/run_yamllint.py --config .yamllint.yaml
 
 printf -- "\n--- Broken wikilink check ---\n\n"
-python3 .github/scripts/ci/find_broken_wikilinks.py --target ${ARTICLES}
+python3 scripts/ci/find_broken_wikilinks.py --target ${ARTICLES}
 
 printf -- "\n--- Outdated tag check ---\n\n"
-bash .github/scripts/ci/check_outdated_tags.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
+bash scripts/ci/check_outdated_tags.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
