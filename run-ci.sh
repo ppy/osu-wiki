1d2f0c07d7de05dc20ba8c3ed45443556f6fe335
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-04-25 20:50:24 +0200

    remove surrounding newlines in output

--- run-ci.sh
+++ run-ci.sh
@@ -4,19 +4,17 @@ FIRST_COMMIT_HASH=$( git log master..$( git branch --show-current ) --pretty=for
 LAST_COMMIT_HASH=$( git rev-parse HEAD )
 ARTICLES=$( git diff --diff-filter=d --name-only ${FIRST_COMMIT_HASH}^ ${LAST_COMMIT_HASH} "wiki/**/*.md" "news/*.md" )
 
-printf "\n--- Binary file size check ---\n\n"
+printf -- "--- Binary file size check ---\n\n"
 bash .github/scripts/ci/inspect_binary_file_sizes.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
 
-printf "\n--- Run remark ---\n\n"
+printf -- "\n--- Run remark ---\n\n"
 bash .github/scripts/ci/run_remark.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
 
-printf "\n--- Run yamllint ---\n\n"
+printf -- "\n--- Run yamllint ---\n\n"
 python3 .github/scripts/ci/run_yamllint.py --config .yamllint.yaml
 
-printf "\n--- Broken wikilink check ---\n\n"
+printf -- "\n--- Broken wikilink check ---\n\n"
 python3 .github/scripts/ci/find_broken_wikilinks.py --target ${ARTICLES}
 
-printf "\n--- Outdated tag check ---\n\n"
+printf -- "\n--- Outdated tag check ---\n\n"
 bash .github/scripts/ci/check_outdated_tags.sh ${FIRST_COMMIT_HASH} ${LAST_COMMIT_HASH}
-
-printf "\n"
