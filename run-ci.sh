5fed941847ad67497674b521414725e12c60d24f
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-05-03 17:00:58 +0200

    clarify usage and prevent accidental runs on master

--- run-ci.sh
+++ run-ci.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# This runs CI checks for committed and unstaged files, but not untracked files.
+# For newly added files, stage or commit them before running these checks.
+
+CURRENT_BRANCH=$( git branch --show-current )
+if test ${CURRENT_BRANCH} == 'master'; then
+  printf -- "Please run this from a feature branch, i.e. not 'master'.\n"
+  exit 1
+fi
+
 python="python3"
 if test -x "./.venv/bin/python3"; then
   python="./.venv/bin/python3"
 fi
 
-FIRST_COMMIT_HASH=$( git log master..$( git branch --show-current ) --pretty=format:%H | tail -1 )
+FIRST_COMMIT_HASH=$( git log master..${CURRENT_BRANCH} --pretty=format:%H | tail -1 )
 ARTICLES=$( git diff --diff-filter=d --name-only ${FIRST_COMMIT_HASH}^ "wiki/**/*.md" "news/*.md" )
 
 printf -- "--- Binary file size check ---\n\n"
