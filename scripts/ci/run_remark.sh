7e527ff139475bfda26f38b15b6d4590b07e13ac
clayton <me@clayton.cc>
2022-07-04 22:40:00 -0700

    Use env instead of argument to check for actions context

--- scripts/ci/run_remark.sh
+++ scripts/ci/run_remark.sh
@@ -3,19 +3,18 @@
 set -e
 set -u
 
+if test $# -lt 1 -o $# -gt 2; then
+  echo "Usage: $0 <first-commit> [<last-commit>]" >&2
+  exit 1
+fi
+
 # https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
 NOTICE_PREFIX="\033[0;90mNotice:\033[m "
 REPORTER=vfile-reporter-position
 
-if test "${1:-}" = --github-action; then
+if test "${GITHUB_ACTIONS:-}"; then
   NOTICE_PREFIX=::notice::
   REPORTER=vfile-reporter-github-action
-  shift
-fi
-
-if test $# -lt 1 -o $# -gt 2; then
-  echo "Usage: $0 [--github-action] <first-commit> [last-commit]" >&2
-  exit 1
 fi
 
 FIRST_COMMIT_HASH="$1"
